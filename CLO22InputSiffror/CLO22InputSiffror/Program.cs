b0cc7d7db0dce0cd73b11df2183e022f38c77447
Marcus Medina <medina.jobb@outlook.com>
2022-09-30 08:14:32 +0200

    Drop files rejected by .gitignore

---
+++ CLO22InputSiffror/CLO22InputSiffror/Program.cs
@@ -0,0 +1,18 @@
+﻿// Heltal = int = int32
+
+// Deklarera och tilldela värdet 0
+int age = 0;
+
+Console.WriteLine("-----------------------------------------------------------------------");
+
+Console.WriteLine("Ange din ålder");
+string input = Console.ReadLine();
+
+age = int.Parse(input);
+float days = 365.25f * age;
+Console.WriteLine("Du har levt " + days + " dagar");
+
+Console.WriteLine("-----------------------------------------------------------------------");
+
+Console.WriteLine("Skriv en siffra");
+float bredd = float.Parse(Console.ReadLine());
---
+++ CLO22InputSiffror/README.md
@@ -0,0 +1,1 @@
+# CLO22InputSiffror
