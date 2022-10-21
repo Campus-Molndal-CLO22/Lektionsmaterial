8e269e4d96d648638d5de82839da2d6e861d4df3
Marcus Medina <medina.jobb@outlook.com>
2022-10-21 14:08:31 +0200

    Städade mappen

--- WebApplication3/WebApplication3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
